780d15743885b511a28978c8bcd1160db8d3eb20
Ram <140935795+tcker@users.noreply.github.com>
2024-06-08 18:30:12 +0800

    Create overflow.css

---
+++ CSS/overflow.css
@@ -0,0 +1,14 @@
+/* Keyword values */
+overflow: visible;
+overflow: hidden;
+overflow: clip;
+overflow: scroll;
+overflow: auto;
+overflow: hidden visible;
+
+/* Global values */
+overflow: inherit;
+overflow: initial;
+overflow: revert;
+overflow: revert-layer;
+overflow: unset;
